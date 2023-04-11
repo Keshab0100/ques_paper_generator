15033fd3977e1a91bf48fa0f484d8a44676b81b5
Keshab <www.keshab.kk@gmail.com>
2023-04-11 16:06:42 +0530

    full funtioning

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-import { config } from "dotenv"
-config()
-
-import { Configuration, OpenAIApi } from "openai"
-import readline from "readline"
-
-const openAi = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
-  })
-)
-
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
-
-userInterface.prompt()
-userInterface.on("line", async input => {
-  const response = await openAi.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choice)
-  userInterface.prompt()
-})
-=======
 import { Configuration, OpenAIApi } from "openai"
 import { config } from "dotenv"
-import readline from "readline"
 import express from 'express'
 import cors from "cors"
 import bodyParser from "body-parser"
@@ -50,6 +21,8 @@ app.post("/", async (req, res) => {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: req.body.prompt }],
   })
+  console.log("sucessfully")
+  console.log(response.data.choices[0].message.content)
   res.status(200).send(response.data.choices[0].message.content)
 });
 
@@ -57,4 +30,3 @@ app.post("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
 });
->>>>>>> b08d4fa48518151c1091e09f67e0b70cf9b45664
--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,67 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
+import axios from "axios";
+import ReactLoading from "react-loading";
 
 const Card = () => {
+  const [subject, setSubject] = useState();
+  const [topic, setTopic] = useState();
+  const [type, setType] = useState();
+  const [quant, setQuantity] = useState();
+  const [Data, setData] = useState();
+  const [flag, setFlag] = useState(false);
+  const getques = async () => {
+    setFlag(true);
+    const req = `create ${quant} ${type} questions based on ${subject} and the following topics ${topic} in json format`;
+    try {
+      const res = await axios.post("http://localhost:8000", { prompt: req });
+      console.log(res.data);
+      setData(res.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div className="form-parent">
-
-    <form className="form-container">
-      <label for="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Enter your name"
-        className="form-input"
-      />
-
-      <label for="email">Email:</label>
-      <input
-        type="text"
-        id="email"
-        name="email"
-        placeholder="Enter your email"
-        className="form-input"
-        />
-
-      <label for="phone">Phone:</label>
-      <input
-        type="text"
-        id="phone"
-        name="phone"
-        placeholder="Enter your phone number"
-        className="form-input"
-        />
-
-      <label for="message">Message:</label>
-      <input
-        type="text"
-        id="message"
-        name="message"
-        placeholder="Enter your message"
-        className="form-input"
-        />
-
-      <input type="submit" value="Submit" className="form-button" />
-    </form>
+      <div className="form">
+        <div>
+          <label for="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+          />
         </div>
+        <br />
+        <br />
+
+        <div>
+          <label for="topics">Topic Names:</label>
+          <input
+            type="text"
+            id="topics"
+            name="topics"
+            onChange={(e) => {
+              setTopic(e.target.value);
+            }}
+          />
+        </div>
+        <br />
+        <br />
+
+        <div>
+          <label for="num-questions">Number of Questions:</label>
+          <input
+            type="number"
+            id="num-questions"
+            name="num-questions"
+            onChange={(e) => {
+              setQuantity(e.target.value);
+            }}
+          />
+        </div>
+        <br />
+        <br />
+
+        <div>
+          <label for="question-type">Type of Question:</label>
+          <input
+            type="text"
+            id="type"
+            name="Type of question"
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+        </div>
+        <br />
+        <br />
+        <input
+          type="submit"
+          value="Submit"
+          onClick={getques}
+          className="form-button"
+        />
+        <div id="output">
+          {!Data && flag && (
+            <ReactLoading type={"spin"} color={"#000"} height={"5%"} />
+          )}
+          {Data &&
+            Data.questions.map((question, index) => (
+              <div key={index}>
+                <p>
+                  Question {index + 1}: {question.question}
+                </p>
+                {question.options.map((a, i) => (
+                  <div>
+                    {i + 1} : {a}
+                  </div>
+                ))}
+                <p>Answer: {question.answer}</p>
+                <br />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Card;
-{
-  /* <ul className='form-list'>
-                <li>Enter your topics </li> 
-                <input classname="input" type='text'></input>
-
-                <li>Enter number of questions</li>
-                <input  classname="input" type='number'></input>
-            
-                <li>enter marks of each question</li>
-                <input  classname="input" type='number'></input>
-        
-                <li>enter type</li>
-                <input  classname="input" type='text'></input>
-
-            </ul> */
-}
